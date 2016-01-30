9413d9f3069e0c21e9d7048de3ebc4f1adae70e8
Misha <puochta@mail.ru>
2016-01-31 02:14:34 +0900

    Created base project structure.

---
+++ app/src/androidTest/java/com/kerby19912707/buisnessdo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kerby19912707.buisnessdo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
