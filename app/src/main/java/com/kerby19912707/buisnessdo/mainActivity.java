8e56b3d383105996d9111cb623a47e183e6001d2
Misha <puochta@mail.ru>
2016-01-31 04:28:37 +0900

    Create Layout and activity_main

---
+++ app/src/main/java/com/kerby19912707/buisnessdo/mainActivity.java
@@ -0,0 +1,16 @@
+package com.kerby19912707.buisnessdo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Misha on 31.01.2016.
+ */
+public class mainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
